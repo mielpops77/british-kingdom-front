3b8875ec810a3f2edcf05d04ebb97d4e02142983
mielpops77 <amielzeitoun@gmail.com>
2023-10-16 23:23:49 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://localhost:3000/',
+  apiUrl: 'http://localhost:3000/api/',
+  apiUrlImgProfilCat: 'http://localhost:3000/assets/CatsProfil/',
+  apiUrlImgParentsCat: 'http://localhost:3000/assets/CatsParents/',
+  apiUrlImgCat: 'http://localhost:3000/assets/CatsImages/'
+  
 };
