86a476a224e0edf921a2b3d80bb70afd9524c625
mielpops77 <amielzeitoun@gmail.com>
2024-02-06 02:06:20 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,24 @@ export const environment = {
   apiUrl: 'http://localhost:3000/api/',
   apiUrlImgProfilCat: 'http://localhost:3000/assets/CatsProfil/',
   apiUrlImgParentsCat: 'http://localhost:3000/assets/CatsParents/',
-  apiUrlImgCat: 'http://localhost:3000/assets/CatsImages/'
-  
+  apiUrlImgCat: 'http://localhost:3000/assets/CatsImages/',
+  apiUrlImgChaton: 'http://localhost:3000/assets/Chatons/',
+  apiUrlImgDivers: 'http://localhost:3000/assets/imgDivers/',
+  apiUrlImgPedigree: 'http://localhost:3000/assets/Pedigree/',
+
+
+  apiUrlProfil: 'http://localhost:3000/assets/Profil/',
+  apiUrlBanner: 'http://localhost:3000/assets/banner/',
+
+  apiUrlCats: 'http://localhost:3000/api/cats/',
+  apiUrlPorte: 'http://localhost:3000/api/portee/',
+  apiUrlChaton: 'http://localhost:3000/api/chaton/',
+
+  apiUrlLivreOr: 'http://localhost:3000/api/livreOr/',
+  apiUrlContact: 'http://localhost:3000/api/contact/',
+
+  apiUrlFavicon: 'http://localhost:3000/assets/favicon/',
+
+
+  id: 1
 };
